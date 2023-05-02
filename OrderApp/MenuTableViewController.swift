4549ad6b13ac6b4b0e46d0457937096310630c1b
Christian Manzaraz <63519258+Manzaraz@users.noreply.github.com>
2023-05-02 18:47:23 -0300

    Part Five: Categories  • Basic Table Setup  • Pass Category Data

--- OrderApp/MenuTableViewController.swift
+++ OrderApp/MenuTableViewController.swift
@@ -7,7 +7,19 @@
 
 import UIKit
 
+@MainActor
 class MenuTableViewController: UITableViewController {
+    
+    let category: String
+    
+    init?(coder: NSCoder, category: String) {
+        self.category = category
+        super.init(coder: coder)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
